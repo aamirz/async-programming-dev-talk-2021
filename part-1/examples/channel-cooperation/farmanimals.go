282a29435cdb331dacd7f514dd64e80122756be1
Aamir Zainulabadeen <aamirzainulabadeen@Aamirs-MacBook-Pro.local>
2021-04-15 10:05:55 -0500

    finished channels example

---
+++ part-1/examples/channel-cooperation/farmanimals.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+       "fmt"
+       "sync"
+)
+
+func moo(messages chan string, w *sync.WaitGroup) {
+     for i := 0; i < 10; i++ {
+     messages <- "moo"
+     }
+
+    w.Done()
+}
+
+func quack(messages chan string, w *sync.WaitGroup) {
+     for i := 0; i < 10; i++ {
+     messages <- "quack"
+     }
+
+    w.Done()
+}
+
+func scree(messages chan string, w *sync.WaitGroup) {
+     for i := 0; i < 10; i++ {
+     messages <- "scree"
+     }
+
+    w.Done()
+}
+
+
+func megaphone(messages chan string, done chan bool) {
+     for {
+     message, more  := <-messages
+     if (more) {
+        fmt.Printf("%s\n", message)
+     } else {
+        fmt.Println("printed all messages")
+        break
+}
+}
+    done <- true
+
+}
+
+func main() {
+     var w sync.WaitGroup
+     messages := make(chan string, 2)
+     done := make(chan bool, 1)
+
+     w.Add(3)
+     go moo(messages, &w)
+     go quack(messages, &w)
+     go scree(messages, &w)
+
+     go megaphone(messages, done)
+     w.Wait()
+     close(messages)
+
+     <-done
+}
