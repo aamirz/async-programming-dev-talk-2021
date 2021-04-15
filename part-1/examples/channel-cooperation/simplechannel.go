3a0ac48ceb3a8406621e54ce1549f7cc2341161f
Aamir Zainulabadeen <aamirzainulabadeen@Aamirs-MacBook-Pro.local>
2021-04-15 13:31:39 -0500

    simple channel with text

---
+++ part-1/examples/channel-cooperation/simplechannel.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "sync"
+)
+
+func printMessage(messages chan string, w *sync.WaitGroup) {
+    message := <-messages
+    fmt.Println(message)
+    w.Done()
+}
+
+// will this code work?
+func main() {
+    var w sync.WaitGroup
+    messages := make(chan string, 1)
+
+    // wait until a message is available then print it
+    w.Add(1)
+    go printMessage(messages, &w)
+
+    // accept a line of input
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter text: ")
+    reader.ReadString('\n')
+
+    // await the message write before termination
+    w.Wait()
+}
